8f6044b63113191d0d0de6b36bd09bc78b5a5fec
eviegan <wy@starbrothers99.com>
2025-10-03 17:21:08 +0800

    Create db.js

---
+++ server/lib/db.js
@@ -0,0 +1,4 @@
+import pg from 'pg';
+const { Pool } = pg;
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: process.env.DATABASE_URL?.includes('localhost') ? false : { rejectUnauthorized: false } });
+export const q = (text, params=[]) => pool.query(text, params);
